cbdeeeb82d9c9ec043f88160ba4060a4b4c1645c
avin169 <avinirzam@gmail.com>
2020-11-12 19:29:05 +0700

    tes

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import TicTacToe from "./TicTacToe";
+import "papercss/dist/paper.min.css";
+
+function App() {
+  return (
+    <>
+      <Main>
+      <b><h1>permainan Tic Tac Toe</h1></b>
+        <TicTacToe />
+      </Main>
+      <Footer>
+        <FooterInner>
+          Avin Irza Mahendra
+          SP.3.2 / 1119101711{" "}
+          <a href="https://github.com/Clarity-89/React_tic_tac_toe">Github</a>
+        </FooterInner>
+      </Footer>
+    </>
+  );
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.div`
+  padding: 16px 0;
+`;
+export default App;
